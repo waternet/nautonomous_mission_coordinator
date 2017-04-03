bd8ba4d248546a093c8a574df201f58d358090e4
jesse <jessebeekman@gmail.com>
2017-04-03 13:16:47 +0200

    reconfig for planner

--- include/nautonomous_operation_action/operation_manager.h
+++ include/nautonomous_operation_action/operation_manager.h
@@ -8,5 +8,8 @@
 #include "geometry_msgs/Quaternion.h"
 #include <tf/transform_listener.h>
 
+geometry_msgs::Point nextPosition_;
+geometry_msgs::Quaternion nextOrientation_;
+
 #endif /* OPERATIONMANAGER_H_ */
 
--- src/operation_manager.cpp
+++ src/operation_manager.cpp
@@ -1,4 +1,5 @@
 #include "../include/nautonomous_operation_action/operation_manager.h"
+#include "../include/nautonomous_navigation_planner/AddTwoInts.h"
 
 using namespace std;
 
@@ -10,6 +11,12 @@ int main(int argc, char** argv){
 	ros::init(argc, argv, "move_base_action_client");
 	ros::NodeHandle nh;
 
+	/* Autonomous */
+	ros::ServiceClient client = nh.serviceClient<nautonomous_navigation_planner::AddTwoInts>("add_two_ints");
+	nautonomous_navigation_planner::AddTwoInts srv;
+	srv.request.c = 52.36905;
+	srv.request.d = 4.89248;
+
 	MissionServer server(nh,"mission_action");
 	MoveBaseActionClient moveBase = MoveBaseActionClient();
 
@@ -18,11 +25,34 @@ int main(int argc, char** argv){
 
 	ros::spinOnce();
 	while(ros::ok()){
-		ROS_INFO("test...");
+		
 		//Simulate goal order (position and orientation)
+
+		//Manual coordinates 
+		
+		/*
 		server.getNextGoal();
 		moveBase.requestGoal(server.nextPosition_, server.nextOrientation_);
+		*/
 
+		/* Autonomous */
+
+		if(client.call(srv)) {
+			nextPosition_ = geometry_msgs::Point();
+			nextPosition_.x = srv.response.x;
+			nextPosition_.y = srv.response.y;
+			nextPosition_.z = 0;
+	
+			nextOrientation_ = geometry_msgs::Quaternion();
+			nextOrientation_.x = 0.0;
+			nextOrientation_.y = 0.0;
+			nextOrientation_.z = srv.response.lat;
+			nextOrientation_.w = srv.response.lon;
+			moveBase.requestGoal(nextPosition_, nextOrientation_);
+		}
+
+		
+		
 		r.sleep();
 		ros::spinOnce();
 	}
